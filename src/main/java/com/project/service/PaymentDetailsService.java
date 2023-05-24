27fa05ce59180f9497d541a05d8d74cd7aa2bd81
Hitesh <hitesh.kolte03@gmail.com>
2023-05-24 12:33:40 +0530

    task number 152

---
+++ src/main/java/com/project/service/PaymentDetailsService.java
@@ -0,0 +1,12 @@
+package com.project.service;
+/*
+ *task no 152 Design the resful web service  for Nominee delete  operation  
+ * @Author Hitesh.
+ */
+import com.project.model.PaymentDetails;
+
+
+public interface PaymentDetailsService {
+
+	public PaymentDetails savePaymentDetails (PaymentDetails paymentDetails );
+}
